9433893074b706bce7f2d76fe0025b80d464c390
Shoham Baris <shoham.baris@gmail.com>
2025-01-02 11:17:40 +0200

    better curve fit

--- photoelasticity/fit_curve.py
+++ photoelasticity/fit_curve.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy
 from matplotlib import pyplot as plt
+from scipy.optimize import Bounds
 from scipy.signal import argrelmax
 
 from photoelasticity.data_processing import moving_average, process_data
@@ -27,37 +28,35 @@ def find_fit_params(data: np.array, image_title: str, override_maximas_count=Non
 
     if override_maximas_count is None:
         maximas_count = get_maximas_count(data)
-        guess_ = [90, maximas_count]
-        assumed_function = assumed_function_without_offset
     else:
         maximas_count = override_maximas_count
-        offset_guess = 50
-        guess_ = [90, maximas_count, offset_guess]
-        assumed_function = assumed_function_with_offset
 
-    results, _ = scipy.optimize.curve_fit(assumed_function, relative_indicies, data,
-                                          p0=guess_)
+    results, _ = scipy.optimize.curve_fit(assumed_function_with_offset,
+                                          relative_indicies,
+                                          data,
+                                          p0=[90, maximas_count, 30],
+                                          bounds=((30, 1, 0), (100, 20, 40)))
     I0, A, *_ = results
+    fitted_data = assumed_function_with_offset(relative_indicies, *results)
 
     print(f"""
     fitted result is:
         I0:{I0}
         A:{A}
 """)
-    plot_figure(A, data, image_title, relative_indicies, results)
+    plot_figure(A, data, image_title, relative_indicies, fitted_data)
 
 
 def get_maximas_count(data):
     averaged_data = moving_average(data, 30)
-    minimas_count = len(argrelmax(averaged_data, order=50)[0]) + 5
+    minimas_count = len(argrelmax(averaged_data, order=50)[0]) + 3
     return minimas_count
 
 
-def plot_figure(A, data, image_title, relative_indicies, results):
+def plot_figure(A, data, image_title, relative_indicies, fitted_data):
     title = f"Fit for {image_title.split('.')[0]}"
     plt.figure(dpi=400)
     plt.suptitle(title, fontsize=10)
-    fitted_data = assumed_function_without_offset(relative_indicies, *results)
     plt.plot(relative_indicies, data, label="Measured data", linewidth=0.5)
     plt.plot(relative_indicies, fitted_data, label=f"Fitted (A={A:.3f})", linewidth=0.5)
     plt.legend(loc='upper right')
