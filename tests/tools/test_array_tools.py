cc890528dc4014bba2e3948f9b25ab499c056c34
Shoham Baris <shoham.baris@gmail.com>
2025-01-16 22:56:17 +0200

    init uv

--- tests/tools/test_array_tools.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-from photoelasticity.tools.array_tools import moving_average
-
-
-def test_moving_average():
-    avg = moving_average(np.array([1, 2, 3, 4, 5]), 2)
-    assert np.allclose(avg, [1.5, 2.5, 3.5, 4.5])
